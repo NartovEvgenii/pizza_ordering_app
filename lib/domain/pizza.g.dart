ddee45f2c6fed877ea242d54b73a4ec826a03141
evgen <evgeniynartov36rus@gmail.com>
2023-03-11 15:54:39 +0300

    add pizza and cart page

--- lib/domain/pizza.g.dart
+++ lib/domain/pizza.g.dart
@@ -1,10 +1,10 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
+
 part of 'pizza.dart';
 
 // **************************************************************************
 // TypeAdapterGenerator
 // **************************************************************************
-
 
 class PizzaAdapter extends TypeAdapter<Pizza> {
   @override
